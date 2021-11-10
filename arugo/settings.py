12bb349952a9d60c192947afc3dffc6c827e0fef
polarity-cf <futanaristic@gmail.com>
2021-11-10 16:36:27 +0700

    weirdga

--- arugo/settings.py
+++ arugo/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-l*%j4r(s6ab1^=wy5^w5_v+q88a)#1sr_%of5$9z@eoj!odh^v"
+# SECRET_KEY = "django-insecure-l*%j4r(s6ab1^=wy5^w5_v+q88a)#1sr_%of5$9z@eoj!odh^v"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
