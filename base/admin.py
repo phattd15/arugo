8a299868456f0beb5c246dcfc5d2412d18a209b2
polarity-cf <futanaristic@gmail.com>
2021-11-16 00:37:06 +0700

    register to the database

--- base/admin.py
+++ base/admin.py
@@ -2,8 +2,9 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import AuthQuery, Problem, Profile
+from .models import AuthQuery, FetchData, Problem, Profile
 
 admin.site.register(Problem)
 admin.site.register(Profile)
 admin.site.register(AuthQuery)
+admin.site.register(FetchData)
