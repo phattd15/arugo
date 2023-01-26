84c12967272963904e43569f5e8965d456179f5f
PhatTD <tranduyphat01@gmail.com>
2023-01-26 18:45:58 +0900

    Update staticfile

--- arugo/settings.py
+++ arugo/settings.py
@@ -126,12 +126,10 @@ USE_TZ = True
 STATIC_URL = "/static/"
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
-if DEBUG:
-    STATICFILES_DIRS = [
-        os.path.join(BASE_DIR, 'static')
-    ]
-else:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
