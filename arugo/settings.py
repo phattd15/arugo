8e13e53a64794fd385fe7e66045cd4086cd00214
PhatTD <tranduyphat01@gmail.com>
2023-01-26 20:26:20 +0900

    Revert static_root

--- arugo/settings.py
+++ arugo/settings.py
@@ -126,10 +126,8 @@ USE_TZ = True
 STATIC_URL = "/static/"
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
